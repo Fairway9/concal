597155578b528cac51ea13056b9e9ebb036c5416
Fairway9 <digitalmarketingfairway@gmail.com>
2024-10-24 13:58:27 +0700

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,17 @@
 import type { NextConfig } from "next";
+import path from 'path';
 
 const nextConfig: NextConfig = {
+  reactStrictMode: true,
+  webpack: (config) => {
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      '@': path.resolve(__dirname, 'src'),
+    };
+    return config;
+  },
   /* config options here */
+
 };
 
 export default nextConfig;
